c4b0a2768fb12ec92bb635a4ad7810613df1fc14
ashish kumari <ashish.bmninfotech@gmail.com>
2023-10-06 11:11:14 +0530

    diff types of logs with diff color & purpose

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  logger:Logger;
+
+  constructor(){
+    this.logger=new Logger();
+  }
   getHello(): string {
-    console.log("testing get hello");
+    this.logger.log("testing using logger log()");
+    this.logger.error("testing using logger error()");
+    this.logger.warn("testing using logger warn()");
+    this.logger.debug("testing using logger debug()");
+    this.logger.verbose("testing using logger verbose()");
     return 'Hello World!';
   }
   getMsg():string{

740a1466dfa190f4eeb9707cb91ce8595a74b2ea
ashish kumari <ashish.bmninfotech@gmail.com>
2023-10-06 15:42:13 +0530

    added logging Interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerMiddleware } from './logger.middleware';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './logger.inteceptor';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {provide:APP_INTERCEPTOR,
+    useClass:LoggerInterceptor}
+  ],
 })
-export class AppModule implements NestModule{
-  configure(consumer:MiddlewareConsumer){
-    consumer.apply(LoggerMiddleware).forRoutes("*");
-  }
-}
+export class AppModule{}
